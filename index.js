b4824f96ae4ba3fb8d8367bf971c3217c12454a3
monicafp <monikfornaris@gmail.com>
2016-09-10 16:49:18 -0400

    Delete old index file and rename file.js to index

--- index.js
+++ index.js
@@ -3,36 +3,26 @@ var csv = require('csv');
 var prompt = require('prompt-sync')();
 var colors = require('colors');
 
-// ----- MAIN FUNCTIONS ----- //
+
+// ----- REUSABLE FUNCTIONS ----- //
+
+// Process CSV
 function processCsv(filename, callback) {
-  fs.readFile(filename, function(err, data) {
-    if (err) throw err;
-
-    csv.parse(data, function(err, dataParsed) {
-      // triggers when there's an issue parsing the data
-      if (err) throw err;
-      callback(dataParsed);
-    });
-  });
-};
-
-processCsv('./example.csv', callback);
-
-
-//function processCsv(file, callback) {
-  fs.readFile(file, function(error, data) {
+  fs.readFile(filename, function(error, data) {
     if (error) {
       throw error
     };
-    csv.parse(data, function(error, dataParsed) {
+
+    csv.parse(data, { columns: true }, function(error, dataParsed) {
       if (error) {
         throw error
       };
-      callback(dataParsed);
+      callback(dataParsed, filename);
     });
   });
 };
 
+// Write CSV
 function writeCsv(filename, data) {
   csv.stringify(data, { header: true }, function(err, data) {
     if (err) throw err;
@@ -41,114 +31,153 @@ function writeCsv(filename, data) {
       if (error) {
         throw error;
       }
-      console.log('CSV update was successful');
+      console.log('CSV update was successful.');
     });
   });
 };
 
+// Find Contact
+function findContact(data, email) {
+  for (var i = 1; i < data.length; i++) {
+    if (data[i].email === email) {
+      return i;
+    }
+  }
+  return -1;
+};
 
-// ----- FUNCTIONS RELATED TO COMMANDS ----- //
-
-switch(process.argv[2]) {
-  case 'count':
-  callback = addClient
-  break;
+// Contact not found
+function notFound() {
+  console.log("Contact not found.");
 }
 
-function addClient() {
-  console.log(data[1]);
-  var contact = [];
-  console.log("Adding new contact");
-  contact.push(prompt("First Name: ")); contact.push(prompt("Last Name: "));
-  contact.push(prompt("Phone: "));
-  contact.push(prompt("Email: "));
-  contact.push(prompt("City: "));
-  contact.push(prompt("Zip: "));
-  contact.push(prompt("Website: "));
-  contact.push(prompt("Company: "));
-  // llamar a un callback que escribe
+// Contact found
+function found() {
+  console.log("Contact already exists in database.")
+}
+
+// Request Info
+function requestInfo() {
+  var firstName = prompt("First Name: ");
+  var lastName = prompt("Last Name: ");
+  var phone = prompt("Phone: ");
+  var email = prompt("Email: ");
+  var city = prompt("City: ");
+  var zip = prompt("Zip: ");
+  var website = prompt("Website: ");
+  var company = prompt("Company: ");
+
+  var contact = {
+    first_name: firstName,
+    last_name: lastName,
+    phone: phone,
+    email: email,
+    city: city,
+    zipcode: zip,
+    website: website,
+    company: company
+  };
+
+  return contact;
 }
 
 
+// ----- FUNCTIONS FOR EACH CASE IN SWITCH STATEMENT ----- //
 
-// "help" Command
+// "help"
 function help() {
   return "This is a contacts application. What follows is a list of " + "commands".yellow + " you can run, along with their descriptions.\n   " + "search".yellow + "- Search for a contact by email and get their information.\n   " + "add".yellow + "- Add a contact to the list.\n   " + "update".yellow + "- Update a contact\'s information in the list.\n   " + "delete".yellow + "- Delete a contact from the list.\n   " + "count".yellow + "- Get the total count of contacts in the list."
 };
 
-// "search" Command
+// "count"
+function showCount(data) {
+  console.log(data.length - 1);
+}
 
-// "add" Command
-function addContact(data) {
-  var contact = [];
-  console.log("Adding new Contact");
-  contact.push(prompt("First Name: "));    contact.push(prompt("Last Name: "));
-  contact.push(prompt("Phone: "));
-  contact.push(prompt("Email: "));
-  contact.push(prompt("City: "));
-  contact.push(prompt("Zip: "));
-  contact.push(prompt("Website: "));
-  contact.push(prompt("Company: "));
-    // we have to add new client to data
-  writeCsv(data);
+// "add"
+function addContact(data, filename) {
+  var contact = requestInfo();
+  var position = findContact(data, contact.email);
+  if (position === -1) {
+    data.push(contact);
+    writeCsv(filename, data);
+  } else {
+    found();
+  }
 };
 
-// "update" Command
-function updateContact(currentData) {
+// "update"
+function updateContact(data, filename) {
   console.log("Updating Contact: ");
-  var currentContact = prompt("Email: ");
-  var found = false;
-  var update = []
-
-  for (var i = 1; i < currentData.length; i++) {
-    if (currentData[i][3] === currentContact) {
-      found = true;
-      console.log("Enter new data");
-      var name = prompt()
-    }
-  }
-
-  if (!found) {
-      console.log("data not found  " + currentContact);
+  var email = prompt('Please enter contact email to update: ');
+  var position = findContact(data, email);
+  if (position === -1) {
+    notFound();
+  } else {
+    var contact = requestInfo();
+    data.splice(position, 1, contact);
+    writeCsv(filename, data);
   }
 };
 
-// "delete" Command
-function deleteContact(currentData) {
-  console.log("Delete Contact ");
-  var currentContact = prompt("Email: ");
-  var found = false;
-  for (var i = 1; i < currentData.length; i++) {
-    if (currentData[i][3] === currentContact) {
-      found = true;
-      console.log("Contact Deleted");
-      return currentData.splice(i, 1);
-    }
+// "delete"
+function deleteContact(data, filename) {
+  var email = prompt('Please enter contact email to delete: ');
+  var position = findContact(data, email);
+  if (position === -1) {
+    notFound();
+  } else {
+    data.splice(position, 1);
+    writeCsv(filename, data);
   }
-  if (!found) {
-    console.log("Contact " + currentContact + " was not found.");
-    //return currentData;
+};
+
+// "search"
+function searchContact(data) {
+  var email = prompt('Please enter email: ');
+  var position = findContact(data, email);
+  if (position === -1) {
+    notFound();
+  } else {
+    console.log(data[position]);
   }
-}
-
-// "count" Command
-function showCount() {
-  return data.length - 1;
-}
+};
 
 
-// ----- PROGRAM EXECUTION ----- //
+// ----- MAIN PROGRAM ----- //
 
 var argument = process.argv[2];
 
-if (argument === 'count') {
-  processCsv('./example.csv', showCount);
-};
+switch(process.argv[2]) {
+  case 'help':
+    console.log(help());
+    process.exit(-1);
+    break;
 
-if (argument === 'add') {
-  processCsv('./example.csv', addClient);
-};
+  case 'count':
+    callback = showCount;
+    break;
 
-if (argument === 'help') {
-  console.log(help());
-};
+  case 'add':
+    callback = addContact;
+    break;
+
+  case 'update':
+    callback = updateContact;
+    break;
+
+  case 'delete':
+    callback = deleteContact;
+    break;
+
+  case 'search':
+    callback = searchContact;
+    break;
+
+  default:
+    console.log("Command not found.");
+    process.exit(-1);
+    break;
+}
+
+processCsv('./example.csv', callback);
